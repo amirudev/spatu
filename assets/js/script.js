6d226bc4fda4eb58649159daeaab1e6c1c87df45
Wahyu Amirulloh <amirullohw@gmail.com>
2021-02-03 02:03:29 +0700

    dashboard template no chart development

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-// document.getElementById('nav-dropdown').addEventListener('click', () => {
-//     console.log(document.getElementById('nav-dropdown'));
-//     document.getElementById('nav-dropdown').nextSibling.nextSibling.classList.toggle('submenu-hidden');
-// });
-
 document.getElementsByClassName('nav-dropdown');
 var els_nav = document.getElementsByClassName('nav-dropdown');
 Array.prototype.forEach.call(els_nav, (el) => {
